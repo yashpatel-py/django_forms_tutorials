b2cd3e95bf83001a1de1e9a5495f986d5ac140c0
djangoworldyt <djangoworld21@gmail.com>
2023-07-02 15:52:51 +0530

    CharField

--- main/forms.py
+++ main/forms.py
@@ -2,5 +2,37 @@ from django import forms
 from .models import Employee
 
 class EmployeeForm(forms.Form):
-    emp_name = forms.CharField()
+    emp_name = forms.CharField(
+        max_length=50,
+        label="Employee Name",
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        }),
+        initial="Name",
+        help_text="Please enter your full name (First Name Last name)"
+    )
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     emp_salary = forms.IntegerField()
