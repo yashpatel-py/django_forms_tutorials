06055c820501dd0ab49bfdf6c97ff8c6daaf332a
Yash Patel <mailforyash04@gmail.com>
2023-09-16 13:00:04 -0500

    Custom Validation

--- main/forms.py
+++ main/forms.py
@@ -2,56 +2,29 @@ from django import forms
 from .models import Employee, Position
 
 class EmployeeForm(forms.ModelForm):
-    gender_choices = [
-        ("Male", "Male"),
-        ("Female", "Female"),
-        ("Other", "Other")
-    ]
+    class Meta:
+        model = Employee
+        fields = [
+            'emp_name',
+        ]
     
+    def clean_emp_name(self):
+        emp_name = self.cleaned_data['emp_name']
+
+        if emp_name == "":
+            raise forms.ValidationError("The name filed cannot be empty")
+        
+        if emp_name == "Name" or "name":
+            raise forms.ValidationError("Please Enter a valid name")
+        
     # CharField
     emp_name = forms.CharField(
         max_length=50,
         label="Employee Name",
-        required=True,
-        widget=forms.PasswordInput(attrs={
+        required=False,
+        widget=forms.TextInput(attrs={
             'class': 'form-control',
             
         }),
         initial="Name",
     )
-    
-    # EmailField
-    emp_email = forms.EmailField(
-        label = "Employee Email",
-        widget = forms.EmailInput(attrs={
-            'class': 'form-control',
-            'type': 'email',
-            'placeholder': 'example@example.com'
-        }),
-    )
-    
-    # ChoiceField
-    emp_gender = forms.ChoiceField(
-        choices=gender_choices,
-        widget=forms.Select(attrs={
-            'class': 'form-control'
-        })
-    )
-    
-    # ManyToManyField
-    emp_position = forms.ModelMultipleChoiceField(
-        label = 'Employee Position',
-        queryset = Position.objects.all(),
-        widget = forms.CheckboxSelectMultiple(attrs={
-            'class': 'form-check-input'
-        })
-    )
-    
-    class Meta:
-        model = Employee
-        fields = [
-            'emp_name',
-            'emp_email',
-            'emp_gender',
-            'emp_position'
-        ]
--- main/models.py
+++ main/models.py
@@ -8,16 +8,7 @@ class Position(models.Model):
         return self.position_name
 
 class Employee(models.Model):
-    gender_choices = [
-        ("Male", "Male"),
-        ("Female", "Female"),
-        ("Other", "Other")
-    ]
-    
     emp_name = models.CharField(max_length=50, null=False, blank=False)
-    emp_email = models.EmailField(null=True, blank=True)
-    emp_gender = models.CharField(choices=gender_choices, null=True, blank=True, max_length=50)
-    emp_position = models.ManyToManyField(Position, blank=True, null=True)
     
     def __str__(self):
         return self.emp_name
